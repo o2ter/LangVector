067c6a26197864747df9bb700c6913f3811a9fc9
Susan Cheng <susan.doggie@gmail.com>
2024-07-07 14:20:25 +0800

    test

--- index2.mjs
+++ index2.mjs
@@ -10,6 +10,10 @@ const model = await llama.loadModel({
   modelPath: path.join(__dirname, 'models/meta-llama/Meta-Llama-3-8B/ggml-model-q5_k_m.gguf')
 });
 
+console.log(`systemInfo: ${llama.systemInfo}`)
+console.log(`GPU: ${llama.gpu}`)
+console.log(`GPU Devices: ${llama.getGpuDeviceNames()}`)
+
 const context = await model.createContext();
 const session = new LlamaChatSession({
   contextSequence: context.getSequence()
@@ -26,16 +30,23 @@ const options = {
   }
 };
 
-const q1 = "Hi there, how are you?";
-console.log("User: " + q1);
+const questions = [
+  'Hi there, how are you?',
+  'Give me a random number',
+];
 
-let cache = []
+for (const question of questions) {
+  const cache = [];
 
-const a1 = await session.prompt(q1, {
-  ...options,
-  onToken: (token) => {
-    cache.push(...token)
-    console.log(model.detokenize(cache, true))
-  }
-});
-console.log("AI: " + a1);
+  console.log("User: " + question);
+
+  const ans = await session.prompt(question, {
+    ...options,
+    onToken: (token) => {
+      cache.push(...token)
+      console.log(model.detokenize(cache, true))
+    }
+  });
+
+  console.log("AI: " + ans);
+}
