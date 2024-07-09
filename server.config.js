dc70c978c74bd4178e2b665c7a851d375c8ca5d9
Susan Cheng <susan.doggie@gmail.com>
2024-07-09 10:21:15 +0800

    clean

--- server.config.js
+++ server.config.js
@@ -16,7 +16,6 @@ module.exports = (env, argv) => ({
       new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ }),
     ],
     externals: {
-      'mupdf': 'commonjs2 mupdf',
       'node-llama-cpp': 'commonjs2 node-llama-cpp',
     },
   },
--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,4 @@
 //  THE SOFTWARE.
 //
 
-export { PDF } from './pdf';
 export * from './llm';
