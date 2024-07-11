e9c8954bbfa05b2b7e334254b12c1e04c719cab4
Susan Cheng <susan.doggie@gmail.com>
2024-07-11 15:44:41 +0800

    test

--- packages/llama-node/addon.cpp
+++ packages/llama-node/addon.cpp
@@ -38,6 +38,79 @@
 #include "gpuInfo/metal-gpu-info.h"
 #endif
 
+Napi::Value getGpuVramInfo(const Napi::CallbackInfo &info)
+{
+  uint64_t total = 0;
+  uint64_t used = 0;
+
+#ifdef GPU_INFO_USE_CUDA
+  size_t cudaDeviceTotal = 0;
+  size_t cudaDeviceUsed = 0;
+  bool cudeGetInfoSuccess = gpuInfoGetTotalCudaDevicesInfo(&cudaDeviceTotal, &cudaDeviceUsed, logCudaError);
+
+  if (cudeGetInfoSuccess)
+  {
+    total += cudaDeviceTotal;
+    used += cudaDeviceUsed;
+  }
+#endif
+
+#ifdef GPU_INFO_USE_VULKAN
+  uint64_t vulkanDeviceTotal = 0;
+  uint64_t vulkanDeviceUsed = 0;
+  const bool vulkanDeviceSupportsMemoryBudgetExtension = gpuInfoGetTotalVulkanDevicesInfo(&vulkanDeviceTotal, &vulkanDeviceUsed, logVulkanWarning);
+
+  if (vulkanDeviceSupportsMemoryBudgetExtension)
+  {
+    total += vulkanDeviceTotal;
+    used += vulkanDeviceUsed;
+  }
+#endif
+
+#ifdef GPU_INFO_USE_METAL
+  uint64_t metalDeviceTotal = 0;
+  uint64_t metalDeviceUsed = 0;
+  getMetalGpuInfo(&metalDeviceTotal, &metalDeviceUsed);
+
+  total += metalDeviceTotal;
+  used += metalDeviceUsed;
+#endif
+
+  Napi::Object result = Napi::Object::New(info.Env());
+  result.Set("total", Napi::Number::From(info.Env(), total));
+  result.Set("used", Napi::Number::From(info.Env(), used));
+
+  return result;
+}
+
+Napi::Value getGpuDeviceInfo(const Napi::CallbackInfo &info)
+{
+  std::vector<std::string> deviceNames;
+
+#ifdef GPU_INFO_USE_CUDA
+  gpuInfoGetCudaDeviceNames(&deviceNames, logCudaError);
+#endif
+
+#ifdef GPU_INFO_USE_VULKAN
+  gpuInfoGetVulkanDeviceNames(&deviceNames, logVulkanWarning);
+#endif
+
+#ifdef GPU_INFO_USE_METAL
+  getMetalGpuDeviceNames(&deviceNames);
+#endif
+
+  Napi::Object result = Napi::Object::New(info.Env());
+
+  Napi::Array deviceNamesNapiArray = Napi::Array::New(info.Env(), deviceNames.size());
+  for (size_t i = 0; i < deviceNames.size(); ++i)
+  {
+    deviceNamesNapiArray[i] = Napi::String::New(info.Env(), deviceNames[i]);
+  }
+  result.Set("deviceNames", deviceNamesNapiArray);
+
+  return result;
+}
+
 Napi::Value getGpuType(const Napi::CallbackInfo &info)
 {
 #ifdef GPU_INFO_USE_CUDA
@@ -65,6 +138,8 @@ Napi::Object registerCallback(Napi::Env env, Napi::Object exports)
   llama_backend_init();
   exports.DefineProperties({
       Napi::PropertyDescriptor::Function("systemInfo", systemInfo),
+      Napi::PropertyDescriptor::Function("getGpuVramInfo", getGpuVramInfo),
+      Napi::PropertyDescriptor::Function("getGpuDeviceInfo", getGpuDeviceInfo),
       Napi::PropertyDescriptor::Function("getGpuType", getGpuType),
   });
   return exports;
