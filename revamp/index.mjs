817f9f8aa3bc961ea5d0494e06aba56d57b50073
Susan Cheng <susan.doggie@gmail.com>
2024-07-11 15:48:43 +0800

    test

--- revamp/index.mjs
+++ revamp/index.mjs
@@ -1,6 +1,6 @@
 import pkg from 'llama-node';
 
 console.log(pkg.systemInfo())
-// console.log(pkg.getGpuVramInfo())
-// console.log(pkg.getGpuDeviceInfo())
+console.log(pkg.getGpuVramInfo())
+console.log(pkg.getGpuDeviceInfo())
 console.log(pkg.getGpuType())
