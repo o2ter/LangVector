b28e879bd372fb270a0b9931a84370b265ba16d1
Susan Cheng <susan.doggie@gmail.com>
2024-07-11 18:06:45 +0800

    Update index.mjs

--- test2/index.mjs
+++ test2/index.mjs
@@ -1,6 +1,11 @@
 
 import _ from 'lodash';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { LlamaDevice } from '../dist/index.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(LlamaDevice.systemInfo());
 console.log(LlamaDevice.supportsGpuOffloading());
@@ -12,3 +17,7 @@ console.log(LlamaDevice.consts());
 console.log(LlamaDevice.gpuType());
 console.log(LlamaDevice.gpuDeviceInfo());
 console.log(LlamaDevice.gpuVramInfo());
+
+console.log(await LlamaDevice.loadModel({
+  modelPath: path.join(__dirname, 'models', 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
+}));
