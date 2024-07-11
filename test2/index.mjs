b1560158c0424965a164572725bd993bf8e38e50
Susan Cheng <susan.doggie@gmail.com>
2024-07-11 16:47:25 +0800

    test

--- test2/index.mjs
+++ test2/index.mjs
@@ -1,6 +1,6 @@
 
 import _ from 'lodash';
-import { LlamaDevice } from './dist/index.mjs';
+import { LlamaDevice } from '../dist/index.mjs';
 
 console.log(LlamaDevice.systemInfo());
 console.log(LlamaDevice.getSupportsGpuOffloading());
