657a5738e2121e073f7c679cbb604016e596f1af
Susan Cheng <susan.doggie@gmail.com>
2024-07-12 12:50:25 +0800

    Update index.mjs

--- test2/index.mjs
+++ test2/index.mjs
@@ -19,6 +19,6 @@ console.log(LlamaDevice.gpuDeviceInfo());
 console.log(LlamaDevice.gpuVramInfo());
 
 console.log(await LlamaDevice.loadModel({
-  modelPath: path.join(__dirname, 'models', 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
+  modelPath: path.join(__dirname, '../models', 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
   onLoadProgress: progress => console.log({ progress }),
 }));
