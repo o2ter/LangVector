953220f86c017be00fdcfbaf42e15dc833efc12f
Susan Cheng <susan.doggie@gmail.com>
2024-07-14 00:17:20 +0800

    test

--- src/context/llama/types.ts
+++ src/context/llama/types.ts
@@ -37,7 +37,6 @@ export type LlamaContextOptions = {
    * Max number of concurrent sequences. (default to 1)
    */
   sequences?: number;
-  embeddings?: boolean;
   flashAttention?: boolean;
   /**
    * Max number of threads. (default to hardware)
