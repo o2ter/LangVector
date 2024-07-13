0a53080933a78ea944db89c09e22eb6ea0cedde1
Susan Cheng <susan.doggie@gmail.com>
2024-07-13 19:40:06 +0800

    Update index.mjs

--- test2/index.mjs
+++ test2/index.mjs
@@ -7,17 +7,6 @@ import { LlamaDevice } from '../dist/index.mjs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(LlamaDevice.systemInfo());
-console.log(LlamaDevice.supportsGpuOffloading());
-console.log(LlamaDevice.supportsMmap());
-console.log(LlamaDevice.supportsMlock());
-console.log(LlamaDevice.blockSizeForGgmlType(0));
-console.log(LlamaDevice.typeSizeForGgmlType(0));
-console.log(LlamaDevice.consts());
-console.log(LlamaDevice.gpuType());
-console.log(LlamaDevice.gpuDeviceInfo());
-console.log(LlamaDevice.gpuVramInfo());
-
 const model = await LlamaDevice.loadModel({
   modelPath: path.join(__dirname, '../models', 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
   useMmap: true,
@@ -27,20 +16,6 @@ const model = await LlamaDevice.loadModel({
   },
 });
 
-console.log(model.description);
-console.log(model.contextSize);
-console.log(model.meta);
-console.log(model.meta['tokenizer.chat_template']);
-console.log({ ...model.tokens });
-
-console.log(model.tokenize('hello, world'));
-console.log(model.detokenize([15339, 11, 1917]));
-
 const context = model.createContext();
-console.log(context);
 
 console.log(await context.embedding('hello, world'))
-
-const session = context.createSession();
-console.log(session);
-console.log(session.contextSize);
