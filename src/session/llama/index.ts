08f132a2fbf095321463ff6ce3dab10c1a31584d
Susan Cheng <susan.doggie@gmail.com>
2024-07-14 14:45:41 +0800

    Update index.ts

--- src/session/llama/index.ts
+++ src/session/llama/index.ts
@@ -52,6 +52,9 @@ export class LlamaSession extends LLMSession<LlamaDevice, LlamaModel, LlamaConte
   _tokens: number[] = [];
 
   /** @internal */
+  _shift_tokens: number[] = [];
+
+  /** @internal */
   constructor(
     pool: LlamaContext,
     ctx: _LlamaContext,
