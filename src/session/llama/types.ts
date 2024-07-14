caf148eb57ff9ca9e88c2f43b252d49586eff201
Susan Cheng <susan.doggie@gmail.com>
2024-07-14 13:53:14 +0800

    test

--- src/session/llama/types.ts
+++ src/session/llama/types.ts
@@ -23,5 +23,55 @@
 //  THE SOFTWARE.
 //
 
+import { Awaitable } from '@o2ter/utils-js';
+import { ChatHistoryItem, ChatWrapper } from '../../chat/types';
+import type { LlamaSession } from './index';
+
+export type LlamaContextShiftOptions = {
+  /**
+   * The number of tokens to delete from the context window to make space for new ones.
+   * Defaults to 10% of the context size.
+   */
+  size?: (sequence: LlamaSession) => Awaitable<number>;
+  /**
+   * The strategy to use when deleting tokens from the context window.
+   * Defaults to `"eraseFirstResponseAndKeepFirstSystem"`.
+   */
+  strategy?: (options: {
+    chatHistory: ChatHistoryItem[];
+    maxTokensCount: number;
+    tokenizer(text: string, specialTokens?: boolean): Uint32List;
+    chatWrapper: ChatWrapper;
+    lastShiftMetadata?: object | null;
+  }) => Awaitable<{
+    chatHistory: ChatHistoryItem[];
+    metadata?: object | null;
+  }>;
+};
+
 export type LlamaSessionOptions = {
+  contextShift?: LlamaContextShiftOptions;
+};
+
+export type LlamaContextSequenceRepeatPenalty = {
+  /** Tokens to lower the predication probability of to be the next predicted token */
+  punishTokens: Uint32List | (() => Uint32List);
+  /**
+   * The relative amount to lower the probability of the tokens in `punishTokens` by
+   * Defaults to `1.1`.
+   * Set to `1` to disable.
+   */
+  penalty?: number;
+  /**
+   * For n time a token is in the `punishTokens` array, lower its probability by `n * frequencyPenalty`
+   * Disabled by default (`0`).
+   * Set to a value between `0` and `1` to enable.
+   */
+  frequencyPenalty?: number;
+  /**
+   * Lower the probability of all the tokens in the `punishTokens` array by `presencePenalty`
+   * Disabled by default (`0`).
+   * Set to a value between `0` and `1` to enable.
+   */
+  presencePenalty?: number;
 };
--- test2/index2.mjs
+++ test2/index2.mjs
@@ -2,15 +2,18 @@
 import _ from 'lodash';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { getLlama } from 'node-llama-cpp';
+import { getLlama, LlamaChatSession } from 'node-llama-cpp';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const llama = await getLlama();
-const model2 = await llama.loadModel({
+const model = await llama.loadModel({
   modelPath: path.join(__dirname, '../models', 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
 });
-const context2 = await model2.createEmbeddingContext({});
+const context = await model.createContext({});
+const session = new LlamaChatSession({ context });
+
+const context2 = await model.createEmbeddingContext({});
 
 console.log(await context2.getEmbeddingFor('hello, world'))
