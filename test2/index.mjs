c629a3f993ad1fa2fba2a8caa7c25adeb461a9dd
Susan Cheng <susan.doggie@gmail.com>
2024-07-18 17:54:51 +0800

    Update index.mjs

--- test2/index.mjs
+++ test2/index.mjs
@@ -2,7 +2,7 @@
 import _ from 'lodash';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { LlamaDevice } from '../dist/index.mjs';
+import { LlamaDevice, Llama3ChatWrapper } from '../dist/index.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,17 +12,11 @@ const model = await LlamaDevice.loadModel({
   useMmap: true,
 });
 
-console.log(model.applyChatTemplate([
-  { role: "system", content: "You are a helpful assistant" },
-  { role: "user", content: "Hello" },
-  { role: "assistant", content: "Hi there" },
-  { role: "user", content: "How are you?" },
-]))
-
 // console.log(await model.embedding('hello, world'));
 
 const context = model.createContext({
   contextSize: 512,
+  chatWrapper: new Llama3ChatWrapper,
 });
 
 const tokenBias = new Map;
@@ -31,5 +25,7 @@ tokenBias.set(0, 'never');
 for await (const { token, time } of context.prompt('hello, world', { temperature: 0.8, maxTokens: 2, tokenBias })) {
   console.log({ token, time, text: model.detokenize(token, { decodeSpecial: true }) })
 }
+
+console.log(model.detokenize(context.tokens, { decodeSpecial: true }))
 
 console.log(context)
