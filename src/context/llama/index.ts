4f8516d55d59e7225963e7185520a748e855af92
Susan Cheng <susan.doggie@gmail.com>
2024-07-19 23:12:28 +0800

    Update index.ts

--- src/context/llama/index.ts
+++ src/context/llama/index.ts
@@ -363,15 +363,10 @@ export class LlamaContext extends LLMContext<LlamaDevice, LlamaModel> {
           if (!_grammar && !_selected_module && !_.isNil(module_records)) {
             for (const module of _modules) {
               if (tokenStartsWith(_.map(module_records, ([x]) => x), module.beginTrigger)) {
-
                 _selected_module = module;
                 _grammar = _selected_module.grammar();
-                for (const token of module_records) {
-                  _grammar.acceptToken(token);
-                }
-
+                for (const token of module_records) _grammar.acceptToken(token);
                 break;
-
               } else if (module_records.length >= module.beginTrigger.length) {
                 _modules = _.filter(_modules, x => x !== module);
               }
