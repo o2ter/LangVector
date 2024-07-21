a227d678247a1d250132dba8b86c067da838295c
Susan Cheng <susan.doggie@gmail.com>
2024-07-21 17:30:59 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@
 //
 
 export { gbnf } from './chat/grammar/gbnf';
+export { _typeScriptFunctionSignatures } from './chat/grammar/typescript';
 export { schemaToJsonGrammarRules } from './chat/grammar/json';
 export { defineChatSessionFunction } from './context/llama/types';
 export { ChatWrapper } from './chat/wrapper/types';
