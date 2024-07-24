42e91df852a4a66ecf771cf5bd76864bb399627a
Susan Cheng <susan.doggie@gmail.com>
2024-07-24 10:06:36 +0800

    Update test2.mjs

--- test2.mjs
+++ test2.mjs
@@ -36,12 +36,17 @@ const model = await LlamaDevice.loadModel({
   useMmap: true,
 });
 
-const test = 'That is a happy person';
+const test = 'bonjour!';
 
 const list = [
-  'That is a happy dog',
-  'That is a very happy person',
-  'Today is a sunny day',
+  'hello',
+  'hi',
+  'bye',
+  'meta',
+  '你好',
+  'hello, world',
+  'what is your name',
+  '你叫咩名',
 ];
 
 const { vector: v1 } = await model.embedding(test);
