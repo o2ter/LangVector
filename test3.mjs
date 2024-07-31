a0f0d3aa2e698456f319c62284857c3a1c976b1a
Susan Cheng <susan.doggie@gmail.com>
2024-07-31 23:03:15 +0800

    Create test3.mjs

---
+++ test3.mjs
@@ -0,0 +1,97 @@
+//
+//  test.mjs
+//
+//  The MIT License
+//  Copyright (c) 2021 - 2024 O2ter Limited. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import _ from 'lodash';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Llama3ChatWrapper, LlamaDevice, gbnf } from './dist/index.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const model = await LlamaDevice.loadModel({
+  modelPath: path.join(__dirname, 'models', 'meta-llama/Meta-Llama-3.1-8B-Instruct/ggml-model-q3_k_m.gguf'),
+  useMmap: true,
+});
+
+class ChatWrapper extends Llama3ChatWrapper {
+
+  generateSystemMessage(ctx) {
+    return [
+      'You are a helpful, respectful and honest assistant.',
+      'You don\'t answer the question.',
+      'You only need to tell the index of the question provided as below.',
+      'If none of the question matched, please answer 0.',
+      '',
+      '1. What\'s your name?',
+      '2. Where am I?',
+    ].join('\n');
+  }
+}
+
+const context = await model.createContext({
+  contextSize: 4096,
+  chatOptions: {
+    chatWrapper: new ChatWrapper,
+  },
+});
+
+const options = {
+  minP: 0,
+  topK: 40,
+  topP: 0.75,
+  temperature: 0.2,
+  repeatPenalty: {
+    frequencyPenalty: 0.2,
+    presencePenalty: 0.2,
+  },
+  maxTokens: 100,
+};
+
+const grammar = gbnf`[0] | [1-9] [0-9]{0,15}`;
+
+const quests = [
+  'Hi',
+  '你叫咩名?',
+  'What is the time of Hong Kong now?',
+  'What is time offset in Hong Kong?',
+  '頂你呀',
+  'What\'s your favorite color?',
+];
+
+for (const quest of quests) {
+
+  const generator = context.prompt(quest, {
+    ...options,
+    grammar: new LlamaDevice.Grammar(grammar.toString()),
+  });
+  for await (const { response, ...rest } of generator) {
+    console.log({ ...rest, text: model.detokenize(response, { decodeSpecial: true }) });
+  }
+
+  console.log('');
+}
+
+console.log(model.detokenize(context.tokens, { decodeSpecial: true }));
