420730b85dde18e90e6aaf637d4507e88e995cab
Susan Cheng <susan.doggie@gmail.com>
2024-08-23 10:54:48 +0800

    Update grammar.h

--- packages/llama-node/src/grammar.h
+++ packages/llama-node/src/grammar.h
@@ -47,7 +47,7 @@ public:
     rewind(file);
     std::string desc;
     desc.resize(size);
-    fread((char *)desc.c_str(), 1, size, file);
+    (void) fread((char *)desc.c_str(), 1, size, file);
     fclose(file);
     return Napi::String::From(Env(), desc);
   }
