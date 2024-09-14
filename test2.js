772ea8eccc5a45d18c284954d79cee47c504dc1e
Susan Cheng <susan.doggie@gmail.com>
2024-09-14 20:30:50 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -9,4 +9,16 @@ const modelsDir = path.join(__dirname, './models');
     modelPath: path.join(modelsDir, 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
   });
 
+  const context = model.createContext();
+
+  const messages = [
+    'hi',
+  ];
+
+  for (const message of messages) {
+    for await (const res of context.prompt(message)) {
+      console.log(res)
+    }
+  }
+
 })();
