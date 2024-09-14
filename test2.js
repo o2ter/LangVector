d3662b2f0bded82dbcce5f856923c3ad317a698c
Susan Cheng <susan.doggie@gmail.com>
2024-09-14 20:28:42 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,12 @@
-require('./dist/index.js')
+const path = require('path');
+const { LlamaDevice } = require('./dist/index.js');
+
+const modelsDir = path.join(__dirname, './models');
+
+(async () => {
+
+  const model = await LlamaDevice.loadModel({
+    modelPath: path.join(modelsDir, 'meta-llama/Meta-Llama-3-8B-Instruct/ggml-model-q3_k_m.gguf'),
+  });
+
+})();
